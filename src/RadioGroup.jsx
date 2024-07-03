9680bcdb412ad2ef61cca2e6a30585b2a940a1bb
thatguyKacper <kacper.m.wojtowicz@gmail.com>
2024-07-03 22:03:00 +0200

    add more complex logic

--- src/RadioGroup.jsx
+++ src/RadioGroup.jsx
@@ -1,35 +1,42 @@
-import { useState } from "react";
+import { useState } from 'react'
 
 export function RadioGroup({ name, options, onChange }) {
-  const [selectedValue, setSelectedValue] = useState("");
+  const [selectedValue, setSelectedValue] = useState('')
 
   const handleChange = (e) => {
-    setSelectedValue(e.target.value);
+    setSelectedValue(e.target.value)
     if (onChange) {
-      onChange(e.target.value);
+      onChange(e.target.value)
     }
-  };
+  }
 
   return (
     <div
       style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "flex-start",
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'flex-start',
       }}
     >
-      {options.map((option) => (
-        <label key={option}>
+      {options.map((option, index) => (
+        <label
+          key={index}
+          style={option.isPopular ? { border: '1px dashed red' } : null}
+        >
+          {option.icon && <span>{option.icon} </span>}
           <input
             type="radio"
             name={name}
-            value={option}
-            checked={selectedValue === option}
+            value={option.value}
+            checked={selectedValue === option.value}
             onChange={handleChange}
           />
-          {option}
+          {option.label}
+          {option.postfix && <em> {option.postfix}</em>}
+          {selectedValue === option.value && option.details}
+          {option.isPopular ? <strong>🔥 Popular!</strong> : null}
         </label>
       ))}
     </div>
-  );
+  )
 }
